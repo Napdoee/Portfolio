6d7138b4240336ccb312252141098c16331f6920
Napdoee <hidayat0249@gmail.com>
2023-10-05 11:41:46 +0800

    feat: initial commit

---
+++ portfolio-template/src/script.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+      extend: {
+        colors: {
+          primary: '#14b8a6',
+          secondary: '#6b7280',
+          dark: '#0f172a'
+        },
+        screens: {
+            '2xl' : '1320px'
+        }
+      }
+    }
+  }
